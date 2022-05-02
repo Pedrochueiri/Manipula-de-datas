0bc437b52dcb1f49a9f59bff0dbc0bba5cdac351
pedrochueiri <pedrochu99@hotmail.com>
2022-05-02 14:31:43 -0300

    treino2

--- src/br/com/fiap/Praticando2.java
+++ src/br/com/fiap/Praticando2.java
@@ -1,26 +1,32 @@
 package br.com.fiap;
 
+
+
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.JOptionPane;
 
 public class Praticando2 {
 	public static void main(String[] args) {
-		
 		String dataAtual;
 		String dataFinal;
 		
-		dataAtual = JOptionPane.showInputDialog("Digite a data inicial");
-		
+		dataAtual = JOptionPane.showInputDialog("Digite a data inicial");		
 		dataFinal = JOptionPane.showInputDialog("Digite a data Final");
 		
 		LocalDate Atual = LocalDate.parse(dataAtual);
+		LocalDate Final = LocalDate.parse(dataFinal);
+		Period periodo = Period.between(Final, Atual);
 		
-		LocalDate A2 = LocalDate.parse(dataFinal);
+		DateTimeFormatter dft = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		dataAtual = Atual.format(dft);
+		dataFinal = Final.format(dft);
+		String AT = Atual.format(dft);
+		String AF = Final.format(dft);
+
 		
-		Period periodo = Period.between(A2, Atual);
-		
-		JOptionPane.showMessageDialog(null, "Data Atual: " + Atual + "\nData Final: " + A2 + "\nPeriodo: " + periodo.getDays() +"   Dias  " + "" + periodo.getMonths()+"   Meses  " + "" + periodo.getYears()+"  Anos  ");
+		JOptionPane.showMessageDialog(null, "Data Inicial: " + AT  + "\nData Final: " + AF + "\nPeriodo:   " + periodo.getDays() +"  Dias  " + periodo.getMonths()+"   Meses  " + periodo.getYears()+"  Anos  ");
 	}
 }
